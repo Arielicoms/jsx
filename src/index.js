8b82ecdac26d0398ffc2adbe8b331aeeab85c427
Ariel Martinez <ariel@escolarea.com>
2019-03-28 10:05:40 -0600

    update and close mini project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return <div>Hi there!</div>
+function getButtonText(){
+    return 'Click on me!'
 }
 
-ReactDOM.render (
+const App = () => {
+
+    const buttonText = 'Click Me!';
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                Enter name:
+                </label>
+            <input id='name' type='text'></input>
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                {getButtonText()}
+            </button>
+        </div>
+    )
+}
+
+ReactDOM.render(
     <App />,
     document.querySelector('#root')
 )
